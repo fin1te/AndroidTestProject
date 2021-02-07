db37d181ea3fba32126ce23474eac19947e7bd02
fin1te <fin1te@fin1te.github.com>
2021-02-07 21:38:59 +0530

    Done

--- app/src/main/java/com/finite/testproject/MainActivity.java
+++ app/src/main/java/com/finite/testproject/MainActivity.java
@@ -2,13 +2,32 @@ package com.finite.testproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextBtn = findViewById(R.id.nextBtn);
+        nextBtn();
+    }
+
+    private void nextBtn() {
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
